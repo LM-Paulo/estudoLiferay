c34fd9f0ebc23bda32880b9408e64463377fc2ff
paulo <plucasmoraisn@gmail.com>
2023-07-19 11:57:14 -0300

    commit inicial

---
+++ modules/treinamentoMVC/src/main/java/com/liferay/treinamentoMvc/constants/TreinamentoMVCPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.treinamentoMvc.constants;
+
+/**
+ * @author vertigo
+ */
+public class TreinamentoMVCPortletKeys {
+
+	public static final String TREINAMENTOMVC =
+		"com_liferay_treinamentoMvc_TreinamentoMVCPortlet";
+
+}
---
+++ modules/treinamentoMVC/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="treinamentomvc.caption"/></b>
+</p>
